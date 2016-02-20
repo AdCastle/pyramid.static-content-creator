9e9ec5610f2c51154e4917bb7af6ac6966412032
Adam Castle <acastle@ripe.net>
2016-02-20 22:09:44 +0100

    200 response tests

--- static_content_creator/snapshot.py
+++ static_content_creator/snapshot.py
@@ -5,20 +5,39 @@ log = logging.getLogger(__name__)
 
 def url_load(url):
     """
-        param url: Given URL
-        return: content or logged error
-
+    :param url: Given URL
+    :return: url object or logged error
     """
     try:
         return urllib2.urlopen(url,
-                               timeout=1)
+                               timeout=10)
     except urllib2.URLError, e:
-        log.info("There was an error reading from URL: n"
+        log.info("There was an error reading from URL: %s"
                  "\n%r" % (url, e))
     return None
 
 
-def grab_content():
+def check_code(page):
+    """
+    :param: urllib2 object is passed
+    :return: checked code of page if it is a valid response
+    then we return a true value
+    """
+    code = page.getcode()
+    if (code >= 200 <= 206) or (code >= 300 <= 307):
+        return code
+    return None
+
+
+def grab_content(url):
     """
     look up the HTML page and return its content
+    :param url: Given URL
+    :return: HTML content of page
     """
+    load_content = url_load(url)
+    if not load_content:
+        return None
+    if not check_code(load_content):
+        return None
+    return load_content.read()
--- static_content_creator/tests.py
+++ static_content_creator/tests.py
@@ -21,3 +21,20 @@ class ViewTests(unittest.TestCase):
         url = "https://www.example.com/"
         url_content = url_load(url)
         self.assertTrue(url_content)
+        bad_url = "https://www.badurl./"
+        empty_content = url_load(bad_url)
+        self.assertFalse(empty_content)
+
+    def test_check_code(self):
+        from snapshot import check_code
+        from snapshot import url_load
+        url = "https://www.example.com/"
+        url_200 = url_load(url)
+        # valid page
+        self.assertEquals(check_code(url_200), 200)
+
+    def test_grab_content(self):
+        from snapshot import grab_content
+        url = "https://www.example.com/"
+        html = grab_content(url)
+        self.assertTrue("<h1>Example Domain</h1>" in html)
