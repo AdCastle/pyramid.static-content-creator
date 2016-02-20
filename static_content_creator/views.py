f85480702fbab48e935112542848bb6772b2cd66
Adam Castle <acastle@ripe.net>
2016-02-20 21:13:19 +0100

    set up of management screen

--- static_content_creator/views.py
+++ static_content_creator/views.py
@@ -4,3 +4,9 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'static_content_creator'}
+
+
+@view_config(route_name="manage",
+             renderer='templates/manage.pt')
+def management_view(request):
+    return {'project': 'static_content_creator'}
